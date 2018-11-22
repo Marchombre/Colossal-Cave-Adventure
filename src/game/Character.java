2f991b5fd3ab4c720df8a511954d684bbce59dba
Léo <leo.loisy@etu.u-bordeaux.fr>
2018-11-22 21:32:25 +0100

    update Character and Player

--- src/game/Character.java
+++ src/game/Character.java
@@ -6,29 +6,14 @@ import item.Food;
 import item.Item;
 
 public abstract class Character {
-	
-	private String name;
-	private int life;
-	
-	private ArrayList<Item> items;
-	
-	public void addItem(Item i) {
-		this.items.add(i);
-	}
-	
+
+	protected int life;
+
 	public void hit(int h) {
 		this.life = this.life - h;
-	}
-	
-	public void eat(Item i) {
-		if(i instanceof Food) {
-			this.life = ((Food) i).getRegen() + this.life;
-		}
 	}
 	
 	public int getLife() {
 		return this.life;
 	}
-
-
 }
--- src/game/Player.java
+++ src/game/Player.java
@@ -1,13 +1,18 @@
 package game;
 
-import item.Food;
-import item.Item;
+import item.*;
+
+import java.util.ArrayList;
 
 public class Player extends Character {
 
-	public Player(String name) {
-		// TODO Auto-generated constructor stub
+    private static final int MAX_HEALTH = 100;
+    private String name;
+    private ArrayList<Item> items;
 
+	public Player(String heroName) {
+		this.name = heroName;
+		this.life = MAX_HEALTH;
 	}
 
 	public void addItem(Item i) {
