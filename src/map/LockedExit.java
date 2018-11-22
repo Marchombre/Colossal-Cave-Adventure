4cc6f111220b3c1eea6faf8a2182c1a6d19d75ea
Léo <leo.loisy@etu.u-bordeaux.fr>
2018-11-22 19:33:20 +0100

    add DoorKey and updated LockedDoor

--- src/map/LockedExit.java
+++ src/map/LockedExit.java
@@ -1,17 +1,28 @@
 package map;
 
+import game.DoorKey;
+
 public class LockedExit extends Exit {
 
     private boolean locked;
+    private DoorKey key;
 
-    public LockedExit(Place p1, Place p2){
+    public LockedExit(Place p1, Place p2, DoorKey dKey){
         super(p1, p2);
         locked = true;
+        this.key = dKey;
     }
 
-    public void open(Key){
-
+    public void unlock(DoorKey dKey){
+        if(dKey == this.key)
+            this.locked = false;
     }
 
+    @Override
+    public void cross(){
+        if(locked == false){
+            //todo
+        }
+    }
 
 }
