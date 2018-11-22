6a909af385bf6412619ebb8ed4b16ee9e1522f43
Bertrand Arthur <stagiaire.technique@capmonetique.com>
2018-11-22 21:20:33 +0100

    package game + item update

--- src/game/Monster.java
+++ src/game/Monster.java
@@ -3,8 +3,7 @@ package game;
 public class Monster extends Character {
 
 	public Monster(String name, int life) {
-		super(name, life);
-		// TODO Auto-generated constructor stub
+		super(name,life);
 	}
 
 }
